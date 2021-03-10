fd98ee2e44eba4e77f765743d961fbbd6e7ca660
adrien <adrien@xps.pop>
2021-03-10 19:49:38 +0100

    code refactoring

--- discoger/discoger.py
+++ discoger/discoger.py
@@ -2,7 +2,6 @@
 
 import configparser
 import discogs_client
-from discogs_client.exceptions import HTTPError
 import yaml
 import requests
 from bs4 import BeautifulSoup
@@ -18,7 +17,12 @@ from time import sleep
 import telebot
 from telebot import types
 
+import logging
+logging.basicConfig(format='%(asctime)s %(levelname)s - %(message)s', level=logging.INFO)
+
 home = str(Path.home())
+
+url = "https://www.discogs.com/fr/settings/developers"
 
 config = configparser.ConfigParser()
 config_file = Path(home + "/.config/discoger/config.ini")
@@ -30,67 +34,45 @@ else:
     print("No config file")
     exit()
 
-key = config["DEFAULT"]["key"]
-secret = config["DEFAULT"]["secret"]
+schedule_logger = logging.getLogger('schedule')
+logger = telebot.logger
 
 token = config["telegram"]["token"]
 bot = telebot.TeleBot(token)
 chat_id = config["telegram"]["chat_id"]
 
-client = discogs_client.Client('DiscogsAlert/0.1')
-client.set_consumer_key(key, secret)
-
 data_to_save = list()
 
-if config["DEFAULT"]["oauth_token"]:
-    client.set_token(config["DEFAULT"]["oauth_token"], config["DEFAULT"]["oauth_secret"])
-    me = client.identity()
+if config["discogs"]["secret"]:
+    secret = config["discogs"]["secret"]
 else:
-    token, secret, url = client.get_authorize_url()
-
-    print(' == Request Token == ')
-    print(f'    * oauth_token        = {token}')
-    print(f'    * oauth_token_secret = {secret}')
-    print()
-
     print(f'Please browse to the following URL {url}')
 
-    accepted = 'n'
-    while accepted.lower() == 'n':
-        print
-        accepted = input(f'Have you authorized me at {url} [y/n] :')
+    secret = input('Verification code : ')
+    config["discogs"]["secret"] = secret
+    with open(config_file, 'w') as configfile:
+        config.write(configfile)
 
-    oauth_verifier = input('Verification code : ')
-
-    try:
-        access_token, access_secret = client.get_access_token(oauth_verifier)
-    except HTTPError:
-        print('Unable to authenticate.')
-
+try:
+    client = discogs_client.Client('DiscogsAlert/0.1', user_token=secret)
     me = client.identity()
-
-    print
-    print(' == User ==')
-    print(f'    * username           = {me.username}')
-    print(f'    * name               = {me.name}')
-    print(' == Access Token ==')
-    print(f'    * oauth_token        = {access_token}')
-    print(f'    * oauth_token_secret = {access_secret}')
-    print(' Authentication complete. Future requests will be signed with the above tokens.')
+except discogs_client.exceptions.HTTPError as e:
+    logging.error('Error: Unable to authenticate.')
+    raise SystemExit(e)
 
 
 def bot_polling():
     while True:
         try:
-            print("Starting bot polling now. New bot instance started!")
+            logging.info("Starting bot polling now. New bot instance started!")
             bot.polling(none_stop=True, interval=3, timeout=30)
         except Exception as ex:
-            print("Bot polling failed, restarting in {}sec. Error:\n{}".format(30, ex))
+            logging.error("Bot polling failed, restarting in {}sec. Error:\n{}".format(30, ex))
             bot.stop_polling()
             sleep(30)
         else:
             bot.stop_polling()
-            print("Bot polling loop finished.")
+            logging.info("Bot polling loop finished.")
             break
 
 
@@ -106,13 +88,6 @@ What do you want?
 """, reply_markup=markup)
 
 
-@bot.message_handler(commands=['add'])
-def get_add(message):
-    chat_id = message.chat.id
-    msg = bot.send_message(chat_id, "Give me the ID of the release you wanna add in list?")
-    bot.register_next_step_handler(msg, process_add)
-
-
 @bot.message_handler(commands=['check'])
 def get_check(message):
     chat_id = message.chat.id
@@ -120,14 +95,14 @@ def get_check(message):
     check_discogs()
 
 
-def process_add(message):
-    print(message.text)
-
-
 def market_scrape(release_id, title, last_one):
     url = f"https://www.discogs.com/sell/release/{release_id}?output=rss"
     first_last_one = last_one
-    response = requests.get(url)
+    try:
+        response = requests.get(url)
+    except requests.exceptions.RequestException as e:
+        raise SystemExit(e)
+
     soup = BeautifulSoup(response.content, features="xml")
 
     # grab all the summaries and links from discogs marketplace,
@@ -156,7 +131,7 @@ def market_scrape(release_id, title, last_one):
         else:
             new_one = False
     if first_last_one != last_one:
-        print("There are new sale\n")
+        logging.info("There are new sale\n")
         send_msg(title=title, data=this_dict)
     elif not new_one:
         this_dict = {}
@@ -167,9 +142,8 @@ def send_msg(title, data):
     url = data.get("url")
     date = data.get("date")
     price = data.get("price")
-    text = "New release for %s\ndate: %s\nprice: %s\n%s" % (title, date, price, url)
-    url_req = "https://api.telegram.org/bot" + token + "/sendMessage" + "?chat_id=" + chat_id + "&text=" + text
-    requests.get(url_req)
+    text = "New release for :\n%s\ndate: %s\nprice: %s\n%s" % (title, date.strftime('%d %B %Y'), price, url)
+    bot.send_message(chat_id, text)
 
 
 def check_exist(id, database):
@@ -182,18 +156,17 @@ def check_exist(id, database):
 
 
 def check_discogs():
-    print("I'm working...")
     if data_file.exists():
         with open(data_file) as file:
             database = yaml.full_load(file)
     else:
         database = list()
-    id_list = config["DEFAULT"]["id_list"]
+    id_list = config["discogs"]["id_list"]
     list_notif = client.list(id_list)
-    print("Check if there are new a new sale for:")
+    logging.info("Check if there are new a new sale for:")
     for item in list_notif.items:
         last_one = (check_exist(item.id, database))
-        print(str(item.id) + ": " + item.display_title)
+        logging.info(str(item.id) + ": " + item.display_title)
         data_to_save.append(market_scrape(item.id, item.display_title, last_one))
     while {} in data_to_save:
         data_to_save.remove({})
@@ -207,10 +180,10 @@ def run_threaded(job_func):
 
 
 def main():
+    schedule.every(30).minutes.do(run_threaded, check_discogs)
     polling_thread = threading.Thread(target=bot_polling)
     polling_thread.daemon = True
     polling_thread.start()
-    schedule.every().hour.do(run_threaded, check_discogs)
 
     while 1:
         schedule.run_pending()
