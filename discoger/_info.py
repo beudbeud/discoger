c5f0a85f1511d30222ba2431fcba1cad0e9663ad
beudbeud <beudbeud@gmail.com>
2021-03-11 10:23:18 +0100

    Use template pipy

---
+++ discoger/_info.py
@@ -0,0 +1,10 @@
+__version__ = '0.0.1'
+
+__title__ = "Discoger"
+__author__ = 'Beudbeud'
+__author_email__ = 'beudbeud@gmail.com'
+__github_username__ = "beudbeud"
+__copyright__ = "Copyright © 2021 - my name"
+__license__ = 'GNU General Public License v3 (GPLv3)'
+__description__ = "Get notification from Discogs"
+__keywords__ = ["discogs", "telegram", "bot"]
--- setup.py
+++ setup.py
@@ -1,19 +1,39 @@
-#!/usr/bin/env python
+import setuptools
+import discoger._info as package_info
 
-from setuptools import setup
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+with open("requirements.txt", 'r') as fh:
+    requirements = fh.read().splitlines()
 
 setup(
-        name='python3-discogs-alerts',
-        version='1.0.0',
-        description='Get notification from Discogs',
-        long_description='Tols for tracking specifics release and get notifications',
-        url='https://github.com/beudbeud/discogs_alerts',
-        author='beudbeud',
-        author_email='beudbeud@gmail.com',
-        install_requires=open("./requirements.txt", "r").read().split(),
-        scripts=[],
-        entry_points={"console_scripts": ["discoger = discoger.discoger:main"]},
-        packages=['discoger'],
-        package_dir={"discoger": "discoger"},
-        include_package_data=True,
-        )
+    name=package_info.__title__,  # How you named your package folder (MyLib)
+    packages=setuptools.find_packages(),
+    version=package_info.__version__,  # Start with a small number and increase it with every
+    license=package_info.__license__,
+    description=package_info.__description__,  # Give a short description about your library
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author=package_info.__author__,  # Type in your name
+    author_email=package_info.__author_email__,  # Type in your E-Mail
+    url=f'https://github.com/{package_info.__github_username__}/{package_info.__title__}',  #
+    download_url=f'https://github.com/{package_info.__github_username__}/{package_info.__title
+    keywords=package_info.__keywords__,
+    install_requires=requirements,
+    classifiers=[
+        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
+        'Development Status :: 4 - Beta',
+        # Chose either "3 - Alpha", "4 - Beta" or "5 - Production/Stable" as the current state
+        'Programming Language :: Python :: 3',  # Specify which python versions that you want
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+        'Topic :: Bot',
+        'Topic :: Bot :: Telegram',
+        'Topic :: Internet :: WWW/HTTP',
+        'Operating System :: OS Independent'
+    ],
+    python_requires='>=3.6'
+)
